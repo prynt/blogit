354e3e2f003d6057d6381eec900ff922b6583be9
Todd Eichel <todd@toddeichel.com>
2014-08-11 12:45:04 -0400

    Add constraints to archive routes so that edit pages (and probably others) don't get caught by them.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Blogit::Engine.routes.draw do
   # Keep these above the posts resources block
   get "posts/page/:page" => "posts#index"
   get "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
-  get "posts/:year/:month" => 'posts#archives', as: :blog_posts_archives
+  get "posts/:year/:month" => 'posts#archives', as: :blog_posts_archives, constraints: { year: /\d{4}/, month: /\d/ }
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
