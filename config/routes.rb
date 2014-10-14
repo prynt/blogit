104b27307ec9287cf181b9faaa362df18c465733
Todd Eichel <todd@toddeichel.com>
2014-10-14 15:14:34 -0700

    Testing out some routing changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Blogit::Engine.routes.draw do
 
+  get "/" => "posts#index", as: :blog_root
+
   # Keep these above the posts resources block
   get "posts/page/:page" => "posts#index"
   get "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
-  get "posts/:year/:month" => 'posts#archives', as: :blog_posts_archives, constraints: { year: /\d{4}/, month: /\d/ }
+  get "posts/:year/:month" => 'posts#archives', as: :blog_posts_archives, constraints: { year: /\d{4}/, month: /\d{1,2}/ }
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
-
-  get "/" => "posts#index", as: :blog_root
 end
